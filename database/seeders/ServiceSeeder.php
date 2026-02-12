eb7ccde7f698bcb3b0e691e80e587fee87de58f9
nandaarianto <nandaarianto58@gmail.com>
2026-02-13 03:23:18 +0800

    update seeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -22,6 +22,11 @@ class ServiceSeeder extends Seeder
                     'Jaminan Travel',
                     'KTP',
                 ],
+                'form_fields' => [
+                    ['name' => 'nama_lengkap', 'label' => 'Nama Lengkap', 'type' => 'text', 'placeholder' => 'Sesuai KTP', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'negara_tujuan', 'label' => 'Negara Tujuan', 'type' => 'text', 'placeholder' => 'Contoh: Arab Saudi', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'tanggal_berangkat', 'label' => 'Tanggal Berangkat', 'type' => 'date', 'placeholder' => null, 'options_text' => null, 'is_required' => '1'],
+                ],
             ],
             [
                 'slug' => 'izin_ln_obat',
@@ -34,6 +39,11 @@ class ServiceSeeder extends Seeder
                     'Keterangan Dokter',
                     'Paspor',
                 ],
+                'form_fields' => [
+                    ['name' => 'nama_pasien', 'label' => 'Nama Pasien', 'type' => 'text', 'placeholder' => 'Nama lengkap pasien', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'nama_rumah_sakit', 'label' => 'Rumah Sakit Tujuan', 'type' => 'text', 'placeholder' => 'Nama RS di luar negeri', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'diagnosa', 'label' => 'Diagnosa Penyakit', 'type' => 'textarea', 'placeholder' => 'Jelaskan singkat diagnosa', 'options_text' => null, 'is_required' => '1'],
+                ],
             ],
             [
                 'slug' => 'pj_bupati',
@@ -44,6 +54,11 @@ class ServiceSeeder extends Seeder
                     'Usulan Gubernur',
                     'Riwayat Hidup',
                     'SK Pangkat',
+                ],
+                'form_fields' => [
+                    ['name' => 'nama_calon', 'label' => 'Nama Calon PJ', 'type' => 'text', 'placeholder' => 'Nama lengkap calon penjabat', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'nip', 'label' => 'NIP', 'type' => 'text', 'placeholder' => 'Nomor Induk Pegawai', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'jabatan_asal', 'label' => 'Jabatan Saat Ini', 'type' => 'text', 'placeholder' => 'Jabatan sebelum diangkat', 'options_text' => null, 'is_required' => '1'],
                 ],
             ],
             [
@@ -56,6 +71,11 @@ class ServiceSeeder extends Seeder
                     'Bukti Kekerabatan',
                     'Surat Permohonan',
                 ],
+                'form_fields' => [
+                    ['name' => 'nama_pemohon', 'label' => 'Nama Pemohon', 'type' => 'text', 'placeholder' => 'Nama lengkap', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'hubungan_keluarga', 'label' => 'Hubungan Keluarga', 'type' => 'select', 'placeholder' => 'Pilih hubungan', 'options_text' => 'Orang Tua,Anak,Saudara,Pasangan', 'is_required' => '1'],
+                    ['name' => 'keperluan', 'label' => 'Keperluan', 'type' => 'textarea', 'placeholder' => 'Jelaskan keperluan keluarga', 'options_text' => null, 'is_required' => '1'],
+                ],
             ],
             [
                 'slug' => 'cuti_kdh',
@@ -66,6 +86,11 @@ class ServiceSeeder extends Seeder
                     'Surat Permohonan Cuti',
                     'Alasan Cuti',
                     'Jadwal Cuti',
+                ],
+                'form_fields' => [
+                    ['name' => 'nama_pejabat', 'label' => 'Nama Pejabat', 'type' => 'text', 'placeholder' => 'Nama KDH/WKDH', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'lama_cuti', 'label' => 'Lama Cuti (Hari)', 'type' => 'number', 'placeholder' => 'Jumlah hari cuti', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'alasan', 'label' => 'Alasan Cuti', 'type' => 'textarea', 'placeholder' => 'Jelaskan alasan cuti', 'options_text' => null, 'is_required' => '1'],
                 ],
             ],
             [
@@ -78,6 +103,11 @@ class ServiceSeeder extends Seeder
                     'Jadwal Perjalanan',
                     'Surat Tugas',
                 ],
+                'form_fields' => [
+                    ['name' => 'nama_lengkap', 'label' => 'Nama Lengkap', 'type' => 'text', 'placeholder' => 'Nama sesuai paspor', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'instansi', 'label' => 'Instansi', 'type' => 'text', 'placeholder' => 'Nama instansi asal', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'negara_tujuan', 'label' => 'Negara Tujuan', 'type' => 'text', 'placeholder' => 'Masukkan negara tujuan', 'options_text' => null, 'is_required' => '1'],
+                ],
             ],
             [
                 'slug' => 'izin_kerjasama',
@@ -88,6 +118,11 @@ class ServiceSeeder extends Seeder
                     'Draft MoU',
                     'Kajian Kerjasama',
                     'Profil Mitra',
+                ],
+                'form_fields' => [
+                    ['name' => 'nama_kerjasama', 'label' => 'Judul Kerjasama', 'type' => 'text', 'placeholder' => 'Judul MoU/Kerjasama', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'mitra', 'label' => 'Nama Mitra', 'type' => 'text', 'placeholder' => 'Nama pihak mitra kerjasama', 'options_text' => null, 'is_required' => '1'],
+                    ['name' => 'durasi', 'label' => 'Durasi Kerjasama', 'type' => 'text', 'placeholder' => 'Contoh: 2 Tahun', 'options_text' => null, 'is_required' => '1'],
                 ],
             ],
         ];
@@ -100,6 +135,7 @@ class ServiceSeeder extends Seeder
                     'description' => $data['description'],
                     'icon' => $data['icon'],
                     'is_active' => true,
+                    'form_fields' => $data['form_fields'] ?? null,
                 ]
             );
 
